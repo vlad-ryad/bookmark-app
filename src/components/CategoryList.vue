400e86bb8c23b9d45c265a14ea6b5ae2d85cf6e8
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-16 11:43:00 +0300

    feat(component): добавление компонента CategoryList для отображения категорий

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { useCategoryStore } from '@/stores/categories.store';
+import { onMounted } from 'vue';
+
+const store = useCategoryStore();
+
+onMounted(() => {
+  store.fetchCategories();
+});
+</script>
+
+<template>
+  <ul class="category-list">
+    <li class="list-item" v-for="item in store.categories" :key="item.id">
+      <RouterLink :to="`/${item.alias}`">{{ item.name }}</RouterLink>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.category-list {
+  display: flex;
+  flex-direction: column;
+  gap: 34px;
+  margin: 0;
+  padding: 0;
+}
+.category-list li {
+  list-style: none;
+}
+.list-item {
+  list-style: none;
+}
+.list-item a {
+  text-decoration: none;
+  font-size: 14px;
+  transition: all 0.5s;
+  color: var(--color-fg);
+}
+
+.list-item a:hover,
+.list-item a.active-link {
+  font-size: 24px;
+  font-weight: 700;
+}
+</style>
