a397ab23ba7ebbaad1cf4fb243e02967c43a42cc
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-16 11:42:52 +0300

    feat(component): добавление компонента ButtonIcon для кнопок с иконками

---
+++ src/components/ButtonIcon.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const { size = 32 } = defineProps<{ size?: number }>();
+</script>
+
+<template>
+  <button
+    class="button-icon"
+    :style="{ width: `${size}px`, height: `${size}px` }"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button-icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--color-fg);
+  cursor: pointer;
+  border-radius: 50px;
+  border: none;
+}
+.button-icon:hover {
+  background: var(--color-fg-hover);
+}
+</style>
