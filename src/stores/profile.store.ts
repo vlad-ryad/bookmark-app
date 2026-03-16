72c338fecb691a46c597426b1b08b10377a653ff
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-16 11:44:11 +0300

    feat(store): добавление связи профиля с категориями

--- src/stores/profile.store.ts
+++ src/stores/profile.store.ts
@@ -1,3 +1,15 @@
+import { API_ROUTES, htpp } from '@/api';
+import type { Profile } from '@/interfaces/profile.interface';
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
-export const useProfileStore = defineStore;
+export const useProfileStore = defineStore('profile', () => {
+  const profile = ref<Profile>();
+
+  async function fetchProfile() {
+    const { data } = await htpp.get<Profile>(API_ROUTES.profile);
+    profile.value = data;
+  }
+
+  return { profile, fetchProfile };
+});
