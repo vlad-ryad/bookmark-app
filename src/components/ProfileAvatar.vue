a3eadaddf8327e4866497b8e3ec6d487cc83e983
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-16 09:13:46 +0300

    feat(component): добавление компонента аватара профиля

---
+++ src/components/ProfileAvatar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const { name } = defineProps<{ name: string }>()
+</script>
+
+<template>
+  <div class="profile-avatar">
+    <img
+      class="profile-avatar__image"
+      src="/public/avatar.png"
+      alt="Изображение пользователя"
+    />
+    <div>
+      Привет, <strong>{{ name }}</strong>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.profile-avatar {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  font-size: 18px;
+  margin-bottom: 40px;
+}
+.profile-avatar__image {
+  width: 80px;
+  height: 80px;
+}
+</style>
