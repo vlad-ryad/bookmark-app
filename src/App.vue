a5b7441702a49f6ac56d24a1ee568f31a0178a5d
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-16 09:15:19 +0300

    refactor: обновление главного компонента и точки входа приложения

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import ProfileAvatar from './components/ProfileAvatar.vue';
+import { API_ROUTES } from './api';
+import type { Profile } from './interfaces/profile.interface';
+
+const profile = ref<Profile>();
+
+async function fetchProfile() {
+  const data = await fetch(API_ROUTES.profile);
+  const res = (await data.json()) as Profile;
+  profile.value = res;
+}
+
+onMounted(() => {
+  fetchProfile();
+});
+</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
+  <div class="app">
+    <nav class="nav">
+      <ProfileAvatar v-if="profile" :name="profile.name" />
+    </nav>
+    <main>Контент</main>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  display: flex;
+  min-height: calc(100vh - 140px);
+  gap: 200px;
+  max-width: 1450px;
+  margin: 140px auto 0 auto;
+}
+
+.nav {
+  min-width: 400px;
+}
+</style>
