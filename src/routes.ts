5340138ee803196c0a19713c5ce992a726b33a2b
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-23 14:17:11 +0300

    feat(router): обновлена конфигурация маршрутизации - Добавлены новые маршруты для страниц - Настроены параметры и meta-информация - Подключены компоненты страниц

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import AuthView from './views/AuthView.vue';
+import MainView from './views/MainView.vue';
 
 export const router = createRouter({
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: AuthView,
+    },
+    {
+      path: '/main',
+      component: MainView,
+      name: 'main',
+    },
+  ],
+
   history: createWebHistory(),
 });
