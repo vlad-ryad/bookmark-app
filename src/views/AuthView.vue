2bf84a18c8b6a0c9f105f9058730da15b6e67777
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-23 14:16:55 +0300

    feat(pages): добавлены страницы приложения

---
+++ src/views/AuthView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import ButtonText from '@/components/ButtonText.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function redirectToMain() {
+  router.push({
+    name: 'main',
+  });
+}
+</script>
+
+<template>
+  <div class="auth">
+    <div class="auth__form">
+      <h1 class="auth__header">Bookmarkly</h1>
+      <form class="auth__form">
+        <ButtonText type="submit" @click="redirectToMain">Вход</ButtonText>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.auth {
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
+}
+.auth__header {
+  font-size: 52px;
+  font-weight: 700;
+}
+.auth__form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 52px;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import CategoryList from '../components/CategoryList.vue';
+import ProfileAvatar from '../components/ProfileAvatar.vue';
+</script>
+
+<template>
+  <div class="app">
+    <nav class="nav">
+      <ProfileAvatar />
+      <CategoryList />
+    </nav>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  min-height: calc(100vh - 140px);
+  gap: 200px;
+  max-width: 1450px;
+  margin: 140px auto 0 auto;
+}
+
+.nav {
+  min-width: 400px;
+}
+</style>
