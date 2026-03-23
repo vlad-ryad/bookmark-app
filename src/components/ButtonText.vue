e6c0a6523a5f8d8d4cd1e80eaedf0aea9dbf2324
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-23 14:16:33 +0300

    feat(ui): добавлен компонент ButtonText

---
+++ src/components/ButtonText.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--color-fg);
+  cursor: pointer;
+  border-radius: 50px;
+  height: 50px;
+  padding: 12px 24px;
+  border: none;
+  color: var(--color-bg);
+  font-size: 18px;
+}
+.button:hover {
+  background: var(--color-fg-hover);
+}
+</style>
