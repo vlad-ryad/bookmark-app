0952c7d790e923f75bdf62ade1da61f960a2fdc6
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-31 14:47:52 +0300

    feat(ui): добавлен компонент InputString
    - Поле ввода строки с валидацией
    - Поддержка v-model и событий
    - Стилизация для различных состояний

---
+++ src/components/InputString.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { onMounted, useTemplateRef } from 'vue';
+
+const data = defineModel<string>();
+const input = useTemplateRef<HTMLInputElement>('input');
+
+const { isFocused = false } = defineProps<{ isFocused?: boolean }>();
+
+onMounted(() => {
+  if (isFocused && input.value) {
+    input.value.focus();
+  }
+});
+</script>
+
+<template>
+  <input class="input-string" v-model="data" ref="input" />
+</template>
+
+<style scoped>
+.input-string {
+  border: none;
+  border-bottom: 1px solid var(--color-fg);
+  color: var(--color-fg);
+  font-size: 24px;
+  width: 200px;
+}
+.input-string::placeholder {
+  color: var(--color-inactive);
+}
+</style>
