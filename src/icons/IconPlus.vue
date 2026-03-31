12f3d0ef23454ed652e439ff9a895069e68bf0e3
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-03-31 14:47:42 +0300

    feat(ui): добавлены иконки для приложения
    - Иконки для закладок, категорий, действий
    - SVG иконки в виде Vue компонентов

---
+++ src/icons/IconPlus.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+  >
+    <path
+      d="M8.00001 2.88V13.12M13.12 8.00001L2.88 8.00001"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
