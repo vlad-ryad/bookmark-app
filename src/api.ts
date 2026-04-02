bf0410e73cf2c341ca680636355d1500b63155af
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-04-02 13:48:09 +0300

    feat(api): добавлены методы для аутентификации

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,23 @@
 import axios from 'axios';
+import { useAuthStore } from './stores/auth.store';
 
 export const API_ROUTES = {
   profile: `profile`,
   categories: `categories`,
-  bookmarks: (id: number) => `categories/${id}/bookmarks`
+  bookmarks: (id: number) => `categories/${id}/bookmarks`,
+  auth: {
+    login: `auth/login`,
+    profile: `auth/profile`,
+  },
 };
 
-export const http = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  timeout: 10000,
-});
+export function client() {
+  const authStore = useAuthStore();
+  return axios.create({
+    baseURL: 'http://localhost:3000/api/',
+    timeout: 10000,
+    headers: {
+      Authorization: `Bearer ${authStore.getToken}`,
+    },
+  });
+}
