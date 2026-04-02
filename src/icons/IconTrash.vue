f7921913361700975c1ccadd439e4873af87578b
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-04-02 14:53:27 +0300

    feat(icon): добавлена иконка удаления
    - SVG иконка для кнопки Delete
    - Используется для удаления категорий и закладок~

---
+++ src/icons/IconTrash.vue
@@ -0,0 +1,40 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+  >
+    <path
+      d="M5.34961 2.33333C5.58985 1.65364 6.23807 1.16667 7.00003 1.16667C7.76199 1.16667 8.41021 1.65364 8.65045 2.33333"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path
+      d="M11.9582 3.5H2.0415"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path
+      d="M10.9859 4.95833L10.7176 8.98282C10.6143 10.5315 10.5627 11.3059 10.0581 11.7779C9.55355 12.25 8.77748 12.25 7.22535 12.25H6.77422C5.22208 12.25 4.44602 12.25 3.94143 11.7779C3.43684 11.3059 3.38522 10.5315 3.28197 8.98282L3.01367 4.95833"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path
+      d="M5.5415 6.41667L5.83317 9.33333"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path
+      d="M8.45817 6.41667L8.1665 9.33333"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
