c80f30c2c941a2d76e6a017adc9fa0ffdee55384
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-04-08 13:08:20 +0300

    feat(ui): добавлен компонент ButtonIconBig
    - Увеличенная кнопка-иконка для основных действий
    - Используется для добавления закладок

---
+++ src/components/ButtonIconBig.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="button-icon-big">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button-icon-big {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  cursor: pointer;
+  width: 48px;
+  height: 48px;
+  border-radius: 50px;
+  border: 3px solid var(--color-bg);
+}
+.button-icon:hover {
+  background: var(--color-bg-hover);
+}
+</style>
