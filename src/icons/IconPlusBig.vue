82e76cc10fb6972a4a9bb8c0d6fc5b544cc65d28
vlad-ryad <vladislav.ryadinskiy@logictelecom.ru>
2026-04-08 13:08:04 +0300

    feat(icon): добавлена большая иконка плюса
    - SVG иконка для кнопки добавления закладки
    - Используется в компоненте BookmarkAdd

---
+++ src/icons/IconPlusBig.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="25"
+    viewBox="0 0 24 25"
+    fill="none"
+  >
+    <path
+      d="M11.9998 4.81998V20.18M19.6798 12.5L4.31982 12.5"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
